5561d0ee1b464c02dff2a9fb5069f4ada9d1503a
Krunoslav Domic <krunoslavdomic@gmail.com>
2013-11-11 16:39:59 +0100

    Labosi 4 - ispravak - SVE RADI

--- src/hr/foi/air/g2/vjezbe/MapActivity.java
+++ src/hr/foi/air/g2/vjezbe/MapActivity.java
@@ -9,6 +9,7 @@ import hr.foi.air.g2.vjezbe.types.PoiInfo;
 import java.util.List;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
@@ -32,12 +33,16 @@ public class MapActivity extends FragmentActivity implements OnMyTouchListener {
 	private long WAIT_TIME = 1500;
 	private LatLng clickedPoint;
 	private GoogleMap map;
+	private Context context;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		context = this;
 
 		IPoiSource poiSource = new PoiSourceWebSource();
 
@@ -46,6 +51,8 @@ public class MapActivity extends FragmentActivity implements OnMyTouchListener {
 		MySupportMapFragment fm = (MySupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.mapFragment);
 		map = fm.getMap();
+		
+		fm.setOnMyTouchListener(this);
 
 		int n = pois.size();
 		for (int i = 0; i < n; i++) {
@@ -89,7 +96,7 @@ public class MapActivity extends FragmentActivity implements OnMyTouchListener {
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			Dialog myDialog = new Dialog(getApplicationContext());
+			Dialog myDialog = new Dialog(context);
 			myDialog.setContentView(R.layout.dialog_add_poi);
 			myDialog.setTitle(R.string.poi_add);
 			myDialog.setCancelable(true);
@@ -112,6 +119,7 @@ public class MapActivity extends FragmentActivity implements OnMyTouchListener {
 			if (stopTime - startTime < WAIT_TIME) {
 				handler.removeCallbacks(task);
 			}
+			break;
 		case MotionEvent.ACTION_MOVE:
 			stopTime = event.getEventTime();
 			startTime = event.getEventTime();
